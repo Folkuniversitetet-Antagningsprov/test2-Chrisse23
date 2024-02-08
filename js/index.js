95ec1af75b0db87fb9d04318b07a20fe782923b4
Christoffer Wallenberg <christofferwallenberg@hotmail.com>
2024-02-08 13:28:15 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,34 @@
-// Variabler där HTML-element som har hämtats sparas, används nedan sen.
 const showSumElem = document.querySelector('#showSum');
 const inputFormElem = document.querySelector('#inputForm');
 const friendSumElem = document.querySelector('#friendSum');
 const calculateButton = document.querySelector('#calculateButton');
 
 
+function calculateTip(sum, tip) {
+    const calculatedTip = parseInt(sum) * parseFloat(tip);
+    const total = parseInt(sum) + calculatedTip;
+
+    return total;
+}
+
+function divideTotal(total, numberOfFriends) {
+    const sumDivided = total / parseInt(numberOfFriends);
+    return sumDivided;
+}
 
 function showDividedSum(sum) {
-    // Denna funktion tar emot summan som varje person ska betala och visar den på sidan.
-
-    inputFormElem.classList.toggle('hide'); // Döljer inputfälten.
-    showSumElem.classList.toggle('hide'); // Visar elementet för hur mycket varje person ska betala.
-    friendSumElem.innerHTML = sum + ' kr'; // Lägger till i HTML:en vad varje person ska betala.
+    inputFormElem.classList.toggle('hide');
+    showSumElem.classList.toggle('hide');
+    friendSumElem.innerHTML = sum + ' kr';
 }
 
 calculateButton.addEventListener('click', () => {
- // Denna funktion körs när användaren trycker på knappen "Räkna".
+    const sum = document.querySelector('#sum').value;
+    const numberOfFriends = document.querySelector('#numberOfFriends').value;
+    const tip = document.querySelector('#tip').value;
+
+    const total = calculateTip(sum, tip);
+    const friendSum = divideTotal(total, numberOfFriends);
+
+    //showDividedSum(friendSum);
 });
